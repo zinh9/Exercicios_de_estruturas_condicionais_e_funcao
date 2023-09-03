819b50d573220ced54d4ff46d1431fc807e7980f
zinh9 <127460831+zinh9@users.noreply.github.com>
2023-09-03 10:41:30 -0300

    Create Exe 1.py

---
+++ Exe 1.py
@@ -0,0 +1,10 @@
+def verifica(numero):
+    if numero > 0:
+        print("O numero é positivo")
+    
+    else:
+        print("O numero é negativo")
+
+numero = (float(input("Digite um numero para o programa verificar se ele é positivo ou negativo: ")))
+
+verifica(numero)
